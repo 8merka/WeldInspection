fef080cae2747f4cb297ab5592d59c0086d6a735
Алексей Зотов <93433900+8merka@users.noreply.github.com>
2023-10-10 15:17:08 +0300

    feat: add fields Login and Password

--- Weld Inspection DAL Admin/Models/Inspectors.cs
+++ Weld Inspection DAL Admin/Models/Inspectors.cs
@@ -1,10 +1,12 @@
-﻿namespace Weld_Inspection_DAL_Admin.Models
+namespace Weld_Inspection_DAL_Admin.Models
 {
     public class Inspectors
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string Lastname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateOnly DateOfBirdth { get; set; }
