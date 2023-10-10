a879d149894085f8ca5067c6c533611092e2efa0
Алексей Зотов <93433900+8merka@users.noreply.github.com>
2023-10-10 13:22:23 +0300

    fix: dublicated data on page

--- Weld Inspection API Admin/wwwroot/js/block-colision.js
+++ Weld Inspection API Admin/wwwroot/js/block-colision.js
@@ -4,22 +4,16 @@ var editingBlocks = document.querySelectorAll('.information-block .editing-block
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('click', function () {
         var current = document.querySelector('.select-block a.active');
-        if (current) {
-            current.classList.remove('active');
-            var dataBlock = current.closest('.select-block').querySelector('.data');
-            if (dataBlock) {
-                dataBlock.innerHTML = '';
-            }
-        }
+        current.classList.remove('active');
         this.classList.add('active');
         var index = Array.prototype.indexOf.call(links, this);
         for (var j = 0; j < blocks.length; j++) {
             if (j == index) {
                 blocks[j].style.display = 'inline';
-                console.log(index)
                 GetData(index);
             } else {
                 blocks[j].style.display = 'none';
+                blocks[j].querySelector('.data').innerHTML = '';
             }
         }
         if (index > 6) {
