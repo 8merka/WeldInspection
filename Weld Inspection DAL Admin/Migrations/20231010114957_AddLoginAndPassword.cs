53e9250189f8b6912e831340ce0cfb72361cafd0
Алексей Зотов <93433900+8merka@users.noreply.github.com>
2023-10-10 15:19:30 +0300

    init: add new migration

---
+++ Weld Inspection DAL Admin/Migrations/20231010114957_AddLoginAndPassword.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Weld_Inspection_DAL_Admin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginAndPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Inspectors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Inspectors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Inspectors");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Inspectors");
+        }
+    }
+}
